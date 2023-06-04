9ae05ea401149d232c9d09e55b396626d0538cc0
Marcos Garrafa <garrafam2001@yahoo.com.ar>
2023-06-04 14:10:32 -0300

    first1-commit

--- index.js
+++ index.js
@@ -21,8 +21,14 @@ alert ("su costo es de $"+ jabonRopaAzul1);
 let cant1 =parseInt(prompt("ingrese la cantidad"));
 let resultado1=  jabonRopaAzul1 * cant1
 resultado1 = parseInt(resultado1)
-
-alert ("Ud lleva gastado $"+ resultado1)   
+if ( cant1 >=5){ 
+let descuento1=(resultado1*10)/100
+ resultado1= resultado1-descuento1
+alert ("Ud lleva gastado $"+ resultado1) 
+alert("Ud a ahorrado $" + descuento1)  
+ }else {
+ alert ( "ud lleva gastado $" +" "+ resultado1 )    
+ }
   
 break ;
      case 2:
@@ -30,22 +36,43 @@ alert ("su costo es de $"+ jabonRopaVerde1);
 let cant2 =parseInt(prompt("ingrese la cantidad"));
 let resultado2=  jabonRopaVerde1 * cant2
 resultado2 = parseInt(resultado2)
-alert ("Ud lleva gastado $"+ resultado2)
+if ( cant2 >=5){ 
+    let descuento2=(resultado2*10)/100
+     resultado2= resultado2-descuento2
+    alert ("Ud lleva gastado $"+ resultado2) 
+    alert("Ud a ahorrado $" + descuento2)  
+     }else {
+     alert ( "ud lleva gastado $" +" "+ resultado2 )  
 break;
-     case 3:
+ }
+    case 3:
 alert( "su costo es de $ "+ suavizanteRopa1);
 let cant3 =parseInt(prompt("ingrese la cantidad"));
 let resultado3=  suavizanteRopa1 * cant3
 resultado3 = parseInt(resultado3)
-alert ("Ud lleva gastado $"+ resultado3)
+if ( cant3 >=5){ 
+    let descuento3=(resultado3*10)/100
+     resultado3= resultado3-descuento3
+    alert ("Ud lleva gastado $"+ resultado3) 
+    alert("Ud a ahorrado $" + descuento3)  
+     }else {
+     alert ( "ud lleva gastado $" +" "+ resultado3 )  
 break;
+ }
      case 4:
 alert("su costo es de $"+ desodorantePiso1);
 let cant4 =parseInt(prompt("ingrese la cantidad"));
 let resultado4=  desodorantePiso1 * cant4
 resultado4 = parseInt(resultado4)
-alert ("Ud lleva gastado $"+ resultado4)
+if ( cant4 >=5){ 
+    let descuento4=(resultado4*10)/100
+     resultado4= resultado4-descuento4
+    alert ("Ud lleva gastado $"+ resultado4) 
+    alert("Ud a ahorrado $" + descuento4)  
+     }else {
+     alert ( "ud lleva gastado $" +" "+ resultado4 )  
 break;
+ }
      case 5:
 break ;
 default:
@@ -61,7 +88,7 @@ opcion = parseInt(prompt(" elije la opcion deseada,  \n 1-jabon Ropa Azul 1, \n 
     alert("Ud tiene que esperar a cumplir 18 años para poder comprar")
 }
 
-
+alert ("Gracias por su visita")
 
 
 
