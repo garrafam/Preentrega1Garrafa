e0252e0f08c31bc0eaa66c40945e4cba3ecdd192
Marcos Garrafa <garrafam2001@yahoo.com.ar>
2023-06-21 18:57:22 -0300

    second commit

--- index.js
+++ index.js
@@ -1,94 +1,126 @@
-const jabonRopaVerde1 = 300
-const jabonRopaAzul1 = 300
-const desodorantePiso1 = 50
-const suavizanteRopa1 = 200
-const SALIR=5
+
+const SALIR = 6
+
+const ArrayDeProductos = [];
+const ArrayCarrito = [];
+let codigoUnico = 1
+class producto {
+     constructor(codigo, precio, nombre, tamaño, marca) {
+          this.codigo = codigo
+          this.precio = precio;
+          this.nombre = nombre;
+          this.tamaño = tamaño;
+          this.marca = marca;
+     }
+}
+
+
+const jabonRopaVerde = new producto(codigoUnico++, 300, "jabon ropa verde", "1ltr", "prod suelto");
+ArrayDeProductos.push(jabonRopaVerde)
+const jabonRopaAzul = new producto(codigoUnico++, 300, "jabon ropa Azul", "1ltr", " prod suelto");
+ArrayDeProductos.push(jabonRopaAzul)
+const jabonRopaVerde1 = new producto(codigoUnico++, 1500, "jabon ropa verde", "5ltr", "prod suelto");
+ArrayDeProductos.push(jabonRopaVerde1)
+const jabonRopaAzul1 = new producto(codigoUnico++, 1500, "jabon ropa Azul", "5ltr", " prod suelto");
+ArrayDeProductos.push(jabonRopaAzul1)
+const desodorantePiso1 = new producto(codigoUnico++, 50, "Desodorante de piso", "1ltr", " prod suelto");
+ArrayDeProductos.push(desodorantePiso1)
+const suavizanteRopaBlanco = new producto(codigoUnico++, 200, "Suavizante de ropa blanco", "1ltr", "prod suelto");
+ArrayDeProductos.push(suavizanteRopaBlanco)
+console.log  (producto)
+
+
+
+
+
+
+
+
+const solicitarProductoPor = (nombre) => {
+
+     const filtrado = ArrayDeProductos.filter((el) => el.nombre === nombre);
+     let mensajeAMostrar = '';
+     filtrado.forEach((el) => {
+          mensajeAMostrar += "\n Codigo " + el.codigo + "\n Producto: " + el.nombre + " " + "\n Cantidad" + " " + el.tamaño + "\n Precio: " + " " + el.precio
+     });
+     const codigo = parseInt(prompt(mensajeAMostrar + "\n" + "Ingrese producto para agregar al carrito"));
+     const productoEcontrado = ArrayDeProductos.find(el => el.codigo === codigo);
+     ArrayCarrito.push(productoEcontrado);
+}
+const verCarrito = () => {
+     let mensajeAMostrar = 'Lista de Carrito \n';
+     ArrayCarrito.forEach((el) =>{
+             mensajeAMostrar += "\n" + "Producto: " + el.nombre + " " + "\nPrecio: " + el.precio 
+     });
+     const total = ArrayCarrito.reduce((acumulador,element) => acumulador + element.precio,0)
+     mensajeAMostrar += "\n El total es: " + total
+     
+     
+
+     if ( total >=3000){ 
+          let descuento2=(total*10)/100
+          let total1= total-descuento2           
+          mensajeAMostrar += "\n Descuento" +" "+ descuento2 +"\n Total final" +" "+ total1
+           }else {
+           alert ( "ud lleva gastado $" +" "+ total)}
+    
+     alert(mensajeAMostrar);}
+
 
 let nombreDeUsuario = prompt("ingrese su nombre");
 let apellidoDelUsuario = prompt("ingrese su apellido");
 
-alert ("bienvenido a la plataforma "+ nombreDeUsuario +" " + apellidoDelUsuario)
+     
+     alert("bienvenido a la plataforma " + nombreDeUsuario + " " + apellidoDelUsuario);
+         
 
-let numero=parseInt(prompt("ingrese su edad"))
+let numero = parseInt(prompt("ingrese su edad"));
 
-if ( numero >=18 ){ 
-let opcion = parseInt(prompt(" elije la opcion deseada,  \n 1-jabon Ropa Azul 1, \n 2 jabon Ropa Verde 1 ,\n 3 suavizante ropa 1, \n 4 desosorante de piso 1 ,\n 5 salir "))
-while(opcion != SALIR) {
-switch (opcion){
+if (numero >= 18) {
+
+     let opcion = parseInt(prompt(" elije la opcion deseada,  \n 1-jabon Ropa Azul 1, \n 2 jabon Ropa Verde 1 ,\n 3 suavizante ropa 1, \n 4 desosorante de piso 1 ,\n 5 Ver carrito,\n 6 salir "))
+     while (opcion != SALIR) {
+          switch (opcion) {
+
+               case 1:
+
+                    solicitarProductoPor("jabon ropa Azul");
+
+                    break;
+               case 2:
+                    solicitarProductoPor("jabon ropa verde");
+                    break;
+
+               case 3:
+                    solicitarProductoPor("Suavizante de ropa blanco");
+                    break;
+               case 4:
+                    solicitarProductoPor("Desodorante de piso");
+                    break;
+               case 5:
+                    verCarrito()
+                    break;
+
+               case 5:
+                    break;
+               default:
+                    alert("no esta ingresando un numero correcto");
+                    break;
+          }
+
+     
+
+     opcion = parseInt(prompt(" elije la opcion deseada,  \n 1-jabon Ropa Azul 1, \n 2 jabon Ropa Verde 1 ,\n 3 suavizante ropa 1, \n 4 desosorante de piso 1 ,\n 5 Ver Carrito\n 6  salir"))
+     }
+}
+          
+else { 
+     alert("Ud tiene que esperar a cumplir 18 años para poder comprar")
+}
  
-     case 1:
-alert ("su costo es de $"+ jabonRopaAzul1);
-let cant1 =parseInt(prompt("ingrese la cantidad"));
-let resultado1=  jabonRopaAzul1 * cant1
-resultado1 = parseInt(resultado1)
-if ( cant1 >=5){ 
-let descuento1=(resultado1*10)/100
- resultado1= resultado1-descuento1
-alert ("Ud lleva gastado $"+ resultado1) 
-alert("Ud a ahorrado $" + descuento1)  
- }else {
- alert ( "ud lleva gastado $" +" "+ resultado1 )    
- }
-  
-break ;
-     case 2:
-alert ("su costo es de $"+ jabonRopaVerde1);
-let cant2 =parseInt(prompt("ingrese la cantidad"));
-let resultado2=  jabonRopaVerde1 * cant2
-resultado2 = parseInt(resultado2)
-if ( cant2 >=5){ 
-    let descuento2=(resultado2*10)/100
-     resultado2= resultado2-descuento2
-    alert ("Ud lleva gastado $"+ resultado2) 
-    alert("Ud a ahorrado $" + descuento2)  
-     }else {
-     alert ( "ud lleva gastado $" +" "+ resultado2 )  
-break;
- }
-    case 3:
-alert( "su costo es de $ "+ suavizanteRopa1);
-let cant3 =parseInt(prompt("ingrese la cantidad"));
-let resultado3=  suavizanteRopa1 * cant3
-resultado3 = parseInt(resultado3)
-if ( cant3 >=5){ 
-    let descuento3=(resultado3*10)/100
-     resultado3= resultado3-descuento3
-    alert ("Ud lleva gastado $"+ resultado3) 
-    alert("Ud a ahorrado $" + descuento3)  
-     }else {
-     alert ( "ud lleva gastado $" +" "+ resultado3 )  
-break;
- }
-     case 4:
-alert("su costo es de $"+ desodorantePiso1);
-let cant4 =parseInt(prompt("ingrese la cantidad"));
-let resultado4=  desodorantePiso1 * cant4
-resultado4 = parseInt(resultado4)
-if ( cant4 >=5){ 
-    let descuento4=(resultado4*10)/100
-     resultado4= resultado4-descuento4
-    alert ("Ud lleva gastado $"+ resultado4) 
-    alert("Ud a ahorrado $" + descuento4)  
-     }else {
-     alert ( "ud lleva gastado $" +" "+ resultado4 )  
-break;
- }
-     case 5:
-break ;
-default:
-alert ("no esta ingresando un numero correcto");
-break;
 
 
-}
 
-opcion = parseInt(prompt(" elije la opcion deseada,  \n 1-jabon Ropa Azul 1, \n 2 jabon Ropa Verde 1 ,\n 3 suavizante ropa 1, \n 4 desosorante de piso 1 ,\n 5  salir"))
-}
-}else{ 
-    alert("Ud tiene que esperar a cumplir 18 años para poder comprar")
-}
-
-alert ("Gracias por su visita")
-
+alert("Gracias por su visita")
 
 
